e3f9fdb7e35ebeb4135a73386b6502fcd1150d71
Emmanuel Gumede <okuhle.connect@gmail.com>
2021-02-24 22:58:47 +0200

    initiate express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//Application entry file.
+//Express-js server
+const express = require("express");
+
+const server = express();
+const PORT = 5000;
+
+server.get("/", (req, res) => {
+    res.send("Daily Task Tracker App");
+})
+
+server.listen(PORT, () => {
+    console.log(`Task-Tracker Server started on: http://127.0.0.1:${PORT}`);
+});
