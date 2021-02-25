d1a231ead642914fbfe882be2a785c3b9aa7e6fa
Emmanuel Gumede <okuhle.connect@gmail.com>
2021-02-25 22:42:14 +0200

    added dotenv module

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //Express-js server
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const server = express();
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 
 server.get("/", (req, res) => {
     res.send("Daily Task Tracker App");
 })
 
-server.listen(PORT, () => {
-    console.log(`Task-Tracker Server started on: http://127.0.0.1:${PORT}`);
+server.listen(port, () => {
+    console.log(`Task-Tracker Server started on: http://127.0.0.1:${port}`);
 });
